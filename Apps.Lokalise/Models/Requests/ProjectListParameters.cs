8b0753c4b54ad3d6af067cb9b4391d94dfce962e
Oleksandr Sadaliuk <osadaliuk@codebridge.tech>
2023-03-08 11:13:04 +0200

    removed nullable due to poor mapping

--- Apps.Lokalise/Models/Requests/ProjectListParameters.cs
+++ Apps.Lokalise/Models/Requests/ProjectListParameters.cs
@@ -4,12 +4,12 @@ using Newtonsoft.Json;
 namespace Apps.Lokalise.Models.Requests;
 public class ProjectListParameters
 {
-    public string? FilterTeamId { get; set; }
-    public string? FilterNames { get; set; }
+    public string FilterTeamId { get; set; }
+    public string FilterNames { get; set; }
     [JsonConverter(typeof(BoolToBitConverter))]
-    public bool? IncludeStatistics { get; set; }
+    public bool IncludeStatistics { get; set; }
     [JsonConverter(typeof(BoolToBitConverter))]
-    public bool? IncludeSettings { get; set; }
-    public int? Limit { get; set; }
-    public int? Page { get; set; }
+    public bool IncludeSettings { get; set; }
+    public int Limit { get; set; }
+    public int Page { get; set; }
 }
--- Apps.Lokalise/Models/Responses/Tasks/TaskResponse.cs
+++ Apps.Lokalise/Models/Responses/Tasks/TaskResponse.cs
@@ -5,28 +5,28 @@ public class TaskResponse
     public string Title { get; set; }
     public bool CanBeParent { get; set; }
     public string TaskType { get; set; }
-    public int? ParentTaskId { get; set; }
-    public List<string> ClosingTags { get; set; }
+    public string ParentTaskId { get; set; }
+    public string[] ClosingTags { get; set; }
     public bool DoLockTranslations { get; set; }
     public string Description { get; set; }
     public string Status { get; set; }
     public int Progress { get; set; }
     public string DueDate { get; set; }
-    public int DueDateTimestamp { get; set; }
+    public string DueDateTimestamp { get; set; }
     public int KeysCount { get; set; }
     public int WordsCount { get; set; }
     public string CreatedAt { get; set; }
-    public int CreatedAtTimestamp { get; set; }
-    public int CreatedBy { get; set; }
+    public string CreatedAtTimestamp { get; set; }
+    public string CreatedBy { get; set; }
     public string CreatedByEmail { get; set; }
-    public List<Language> Languages { get; set; }
+    public Language[] Languages { get; set; }
     public string SourceLanguageIso { get; set; }
     public bool AutoCloseLanguages { get; set; }
     public bool AutoCloseTask { get; set; }
     public bool AutoCloseItems { get; set; }
     public string CompletedAt { get; set; }
-    public int? CompletedAtTimestamp { get; set; }
-    public int? CompletedBy { get; set; }
+    public string CompletedAtTimestamp { get; set; }
+    public string CompletedBy { get; set; }
     public string CompletedByEmail { get; set; }
-    public List<string> CustomTranslationStatusIds { get; set; }
+    public string[] CustomTranslationStatusIds { get; set; }
 }
