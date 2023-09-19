447cd521a111ae4ed43084d9fcf69b1eab37ff3a
bZverok <bohdan.zverok@blackbird.io>
2023-09-19 16:15:48 +0300

    added connection validator

---
+++ Apps.Lokalise/Connections/ConnectionValidator.cs
@@ -0,0 +1,34 @@
+﻿using Apps.Lokalise.RestSharp;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
+
+namespace Apps.Lokalise.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    private LokaliseClient Client => new();
+    
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        var request = new LokaliseRequest("/system/languages", Method.Get, authProviders);
+
+        try
+        {
+            var response = await Client.ExecuteWithHandling(request);
+            return new()
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
