e2e6b5592aa171d763ea62805dadafc126f3a123
bzveriok <bohdan.zverok@blackbird.io>
2024-02-08 15:08:24 +0200

    added language key filters

--- Apps.Lokalise/Models/Requests/Projects/ListProjectKeysRequest.cs
+++ Apps.Lokalise/Models/Requests/Projects/ListProjectKeysRequest.cs
@@ -9,20 +9,28 @@ public class ListProjectKeysRequest : ListProjectKeysBaseRequest
     [JsonProperty("disable_references")]
     [Display("Disable references")]
     public bool? DisableReferences { get; set; }
-        
+
     [JsonProperty("include_comments")]
     [Display("Include comments")]
     public bool? IncludeComments { get; set; }
-        
+
     [JsonProperty("include_screenshots")]
     [Display("Include screenshots")]
     public bool? IncludeScreenshots { get; set; }
-        
+
     [JsonProperty("include_translations")]
     [Display("Include translations")]
     public bool? IncludeTranslations { get; set; }
-        
+
     [JsonProperty("filter_key_ids")]
     [Display("Filter Key IDs")]
     public string? FilterKeyIds { get; set; }
+
+    public ListProjectKeysRequest()
+    {
+    }
+
+    public ListProjectKeysRequest(ListProjectKeysBaseRequest request) : base(request)
+    {
+    }
 }
