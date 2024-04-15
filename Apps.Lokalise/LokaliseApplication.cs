d483f42fe1695429950c5accbccd1b3bfbe29e15
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-15 16:31:35 +0300

    Ready for pr

--- Apps.Lokalise/LokaliseApplication.cs
+++ Apps.Lokalise/LokaliseApplication.cs
@@ -3,14 +3,8 @@ using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Lokalise;
 
-public class LokaliseApplication : IApplication, ICategoryProvider
+public class LokaliseApplication : IApplication
 {
-    public IEnumerable<ApplicationCategory> Categories
-    {
-        get => [ApplicationCategory.CatAndTms];
-        set { }
-    }
-    
     public string Name
     {
         get => "Lokalise";
